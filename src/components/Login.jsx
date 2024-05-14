4806b5b6ee5a9c9822b2fb05ba329d66902c5c22
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 15:05:47 +0530

    8

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = ({ handleClick, handleOut, authenticate }) => {
                 </>
             ) : (
                 <>
-                    <p>Login</p>
+                    <p>You are not log in, click here below to log In</p>
                     <button onClick={handleClick}>Login</button>
                 </>
             )}
