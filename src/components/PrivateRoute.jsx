cda7b5aa8b35a4a6b47814585dc5effa83bcedd4
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 15:45:14 +0530

    17

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, authenticated }) => {
-    return (
-        <>
-            {authenticated ? <Component /> : <Navigate to="/login" />}
-        </>
-    );
-}
+const PrivateRoute = ({Component, authenticated }) => {
+  return (authenticated ? <Component /> : <Navigate to="/login" />)
+};
 
 export default PrivateRoute;
